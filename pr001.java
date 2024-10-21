35a79976e79b2cd4c39b2323845f1787cf29e769
safin <geosaf_2005@mail.ru>
2024-10-21 15:46:55 +0700

    Проверка работы с типами данных

--- pr001.java
+++ pr001.java
@@ -1,5 +1,37 @@
 class pr001 {
 	public static void main (String args[]){
 		System.out.println("Первая");
+		//переобразование типов
+		//Целые типы
+		byte b; //8 bit
+		b = -128;
+		b = 127;
+		b = (byte)256;
+		short sn; //16 bit
+		sn =  (short)65536;
+		int in; //32 bit
+		long l; //64 bit
+		in = 200000000;
+		l = in;
+		in = in * in * in;
+		System.out.println(in);
+		l = l * l * l;
+		System.out.println(l);
+		//Веществиные
+		float f; //32 bit
+		double dd; //64 bit
+		//символьный
+		char ch; //16 bit
+		ch = 'A';
+		ch++; ++ch;
+		for (int i = 0; i<68; i++)
+                	System.out.print(ch++ + " ");
+                System.out.println();	
+		//Логический 
+		boolean bool;
+	  	bool = true;
+		bool = false;
+
+
 	} 
 }	
